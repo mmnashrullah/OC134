4d8408cfd14c9511b793aaeab050e9937c425154
mmnashrullah <mmnashrullah@gmail.com>
2022-11-06 09:36:50 +0700

    Create hw33.py

---
+++ HOMEWORK3/hw33.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Nama - nama bulan dalam setahun
+
+Bln = ("Januari", "Februari", "Maret", "April",
+       "Mei", "Juni", "Juli", "Agustus",
+       "September", "Oktober", "November", "Desember")
+
+print(Bln)
