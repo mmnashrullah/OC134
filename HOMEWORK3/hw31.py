c19aed16e2af976a53ecea2b01351b5e044d7898
mmnashrullah <mmnashrullah@gmail.com>
2022-11-06 09:28:42 +0700

    Create hw31.py

---
+++ HOMEWORK3/hw31.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+#•	Domain
+#•	Kingdom
+#•	Phylum
+#•	Class
+#•	Order
+#•	Family
+#•	Genus 
+#•	Species
+#•	Nama lokal
+
+
+DictHewan = {"Domain" : "Eukayota", "Kingdom" : "Animalia",
+             "Phylum" : "Chordata", "Class" : "Mammalia",
+             "Order" : "Carnivora", "Family" : "Felidae",
+             "Genus" : "Felis", "Species" : "Felis catus",
+             "Nama_Lokal": "Kucing Rumah"
+             }
+
+print(DictHewan)
