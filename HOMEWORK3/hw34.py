0f26438add559493c97cedc954084733c6233eca
mmnashrullah <mmnashrullah@gmail.com>
2022-11-06 14:51:05 +0700

    Add files via upload

--- HOMEWORK3/hw34.py
+++ HOMEWORK3/hw34.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Nama - nama basa pada DNA
+# Nama - nama basa pada Hachimonji DNA
 # Adenine, Cytosine, Thymine, Guanine
 
-BNDNA = set("Adenine", "Cytosine", "Thymine", "Guanine")
+BNDNA = set(('Adenine', 'Cytosine', 'Thymine', 'Guanine'))
 
 print(BNDNA)
--- HOMEWORK3/hw35.py
+++ HOMEWORK3/hw35.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# Nama - nama basa pada Hachimonji DNA
+# Nama - nama basa pada Hachimoji DNA
 # Adenine, Cytosine, Thymine, Guanine,
-# 5-Aza-7-deazaguanine, 6-Amino-5-nitropyridin-2-one, Isoguanine,Isocytosine
+# 5-Aza-7-deazaguanine, 6-Amino-5-nitropyridin-2-one, Isoguanine, Isocytosine
 
-fzsHDNA = frozenset("Adenine", "Cytosine", "Thymine", "Guanine",
+fzsHDNA = frozenset(("Adenine", "Cytosine", "Thymine", "Guanine",
                     "5-Aza-7-deazaguanine", "6-Amino-5-nitropyridin-2-one",
-                    "Isoguanine","Isocytosine")
+                    "Isoguanine", "Isocytosine"))
 
 print(fzsHDNA)
