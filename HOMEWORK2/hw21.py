df5b9c6af3800dd7516ca716816fe7742157c724
mmnashrullah <mmnashrullah@gmail.com>
2022-11-06 14:45:11 +0700

    Create hw21.py

---
+++ HOMEWORK2/hw21.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+a = ((20 % 3) != (420 / 6 / 9)) and False
+b = (((-(4 ** 5)) < ((-(3 * 3))))) and (4 % 2) <= (40 - (14 * 2))
+c = ((100 ** 0.5) >= 43) or False
+d = ((2**5)) == ((10 ** 3) or ((1 * 7 * 7)) != (0 + 1 + 3))
+e = (not (not True)) or (False and (not False))
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
