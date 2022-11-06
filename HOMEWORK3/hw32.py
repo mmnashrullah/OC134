81c45846c3af82130beff2b6ad3c27ea4914611c
mmnashrullah <mmnashrullah@gmail.com>
2022-11-06 09:33:25 +0700

    Create hw32.py

---
+++ HOMEWORK3/hw32.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+#•	Nama lokal (Indonesia), 
+#•	Nama latin
+#•	Genus 
+#•	Species
+
+
+ListTumb = ("Sambiloto", "Andrograhis paniculata", 
+            "Andrographis", "paniculata")
+
+print(ListTumb)
