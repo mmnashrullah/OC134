cdb3f4062292a7ab0ca78bde9f1e3f1c138d445c
mmnashrullah <mmnashrullah@gmail.com>
2022-11-06 09:50:55 +0700

    Create hw35.py

---
+++ HOMEWORK3/hw35.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Nama - nama basa pada Hachimonji DNA
+# Adenine, Cytosine, Thymine, Guanine,
+# 5-Aza-7-deazaguanine, 6-Amino-5-nitropyridin-2-one, Isoguanine,Isocytosine
+
+fzsHDNA = frozenset("Adenine", "Cytosine", "Thymine", "Guanine",
+                    "5-Aza-7-deazaguanine", "6-Amino-5-nitropyridin-2-one",
+                    "Isoguanine","Isocytosine")
+
+print(fzsHDNA)
