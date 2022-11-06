5059af3701da40227d2c2df2311af7965439fb95
mmnashrullah <mmnashrullah@gmail.com>
2022-11-06 14:49:00 +0700

    Create hw13.py

---
+++ HOMEWORK1/hw13.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+Nama Tumbuhan: Padi
+Kelompok tumbuhan: Monokotil
+Nama Latin: Oryza sativa
+Nama Famili: Poaceae
+Padi adalah tumbuhan monokotil. Nama latinnya adalah Oryza sativa. Padi merupakan anggota Poaceae
+
+"""
+
+a = input("Nama Tumbuhan: ")
+b = input("Kelompok Tumbuhan: ")
+c = input("Nama Latin: ")
+d = input("Nama Famili: ")
+
+print("%s adalah tumbuhan %s. Nama latinnya adalah %s. %s merupakan anggota %s" %
+      (a, b.casefold(), c, a, d))
